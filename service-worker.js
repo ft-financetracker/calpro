eca44e42d88e8b36b712e479658a03971f12d3d1
ft-financetracker <admin.financetracker@gmail.com>
2026-08-16 23:58:30 +0700

    Add files via upload
    Improve CalPro typography icons and app identity

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'calpro-shell-v2';
-const ASSETS = ['./','./index.html','./css/tokens.css','./css/app.css','./css/ft-theme.css','./js/app.js','./js/config.js','./js/api.js','./manifest.webmanifest','./assets/icons/calpro-icon-512.png','./assets/icons/finance-tracker-192.png'];
+const CACHE = 'calpro-shell-v3';
+const ASSETS = ['./','./index.html','./css/tokens.css','./css/app.css','./css/ft-theme.css','./css/typography.css','./js/app.js','./js/config.js','./js/api.js','./manifest.webmanifest','./assets/icons/calpro-icon-192.png','./assets/icons/calpro-icon-512-v2.png','./assets/icons/favicon-48.png','./assets/icons/apple-touch-icon.png','./assets/icons/finance-tracker-192.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => { if (event.request.method !== 'GET') return; event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => { const copy = response.clone(); caches.open(CACHE).then(cache => cache.put(event.request, copy)); return response; }).catch(() => caches.match('./index.html')))); });
